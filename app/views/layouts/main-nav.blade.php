103201d899f806f7b94a0f98d763757809ca96e8
russ <russjohnson09@gmail.com>
2015-01-22 23:42:21 -0500

    added sidebar

--- app/views/layouts/main-nav.blade.php
+++ app/views/layouts/main-nav.blade.php
@@ -1,19 +1,35 @@
 
-<nav class="sidebar (light)">
+<nav class="sidebar light">
 	<ul>
+		<li><a href="http://stlukemi.org/index.php"
+			class="dd-submenu-title">HOME</a></li>
+		<li><a class="dropdown-toggle"
+			href="http://stlukemi.org/%7Estlukemi/school.php"
+			class="dd-submenu-title">SCHOOL</a>
+			<ul class="dropdown-menu" data-role="dropdown">
+				<li><a href="http://stlukemi.org/%7Estlukemi/why-st-luke.php">WHY
+						US?</a></li>
+				<li><a href="http://stlukemi.org/%7Estlukemi/enrollment.php">ENROLLMENT</a></li>
+				<li><a href="http://stlukemi.org/%7Estlukemi/info-for-parents.php">INFO
+						FOR PARENTS</a></li>
+				<li><a href="http://stlukemi.org/%7Estlukemi/sports-page.php">SPORTS</a></li>
+				<li><a href="http://stlukemi.org/%7Estlukemi/music.php">MUSIC</a></li>
+				<li><a href="http://stlukemi.org/%7Estlukemi/calendar.php">CALENDAR</a></li>
+			</ul>
+		</li>
+
 		<li class="title">Items Group 1</li>
 		<li class="active"><a href="#"><i class="icon-home"></i>Dashboard</a></li>
 		<li class="stick bg-red"><a href="#"><i class="icon-cog"></i>Layouts</a></li>
 		<li class="stick bg-yellow"><a class="dropdown-toggle" href="#"><i
 				class="icon-tree-view"></i>Sub menu</a>
-			<ul class="dropdown-menu" data-role="dropdown">
-				<li><a href="">Subitem 1</a></li>
-				<li><a href="">Subitem 2</a></li>
-				<li><a href="">Subitem 3</a></li>
-				<li class="divider"></li>
-				<li><a href="">Subitem 4</a></li>
-				<li class="disabled"><a href="">Subitem 4</a></li>
-			</ul></li>
+		
+		<li><a href="">Subitem 1</a></li>
+		<li><a href="">Subitem 2</a></li>
+		<li><a href="">Subitem 3</a></li>
+		<li class="divider"></li>
+		<li><a href="">Subitem 4</a></li>
+		<li class="disabled"><a href="">Subitem 4</a></li>
 		<li class="stick bg-green"><a href="#">Thread item</a></li>
 		<li class="disabled"><a href="#">Disabled item</a></li>
 
@@ -28,4 +44,5 @@
 				<li><a href="">Subitem 3</a></li>
 			</ul></li>
 	</ul>
+
 </nav>
