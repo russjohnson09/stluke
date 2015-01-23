87ab7aabdf941f80de6165571b25e29ea4ed0ccc
russ <russjohnson09@gmail.com>
2015-01-22 21:41:51 -0500

    more change.

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,23 +1,29 @@
 <!doctype html>
 <html>
 <head>
-	<meta charset="UTF-8">
-	<title>{{isset($title) ? $title : 'St. Luke'}}</title>
-	
-	<!--<link href="/img/favicon.ico" type="image/x-icon" rel="shortcut icon">  -->
-	<link rel="stylesheet" type="text/css" href="/css/main.css">
-	<link rel="stylesheet" type="text/css" href="/css/media.css">
-	<link rel="stylesheet" type="text/css" href="/css/mediaelementplayer.min.css">
-	<link rel="stylesheet" type="text/css" href="/css/style.css">
+<meta charset="UTF-8">
+<title>{{isset($title) ? $title : 'St. Luke'}}</title>
+
+<!--<link href="/img/favicon.ico" type="image/x-icon" rel="shortcut icon">  -->
+
+<link rel="stylesheet" type="text/css"
+	href="/bower_components/bootstrap/dist/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="/css/main.css">
+<link rel="stylesheet" type="text/css" href="/css/media.css">
+<link rel="stylesheet" type="text/css"
+	href="/css/mediaelementplayer.min.css">
+<link rel="stylesheet" type="text/css" href="/css/style.css">
 </head>
-    <body>
-        <div class="container">
-        	@include('layouts.topbar')
-			@yield('content')
-		</div>
-		
-		<div class="footer">
-			@include('layouts.footer');
-		</div>
-    </body>
+<body>
+	<div class="container">@include('layouts.topbar') @yield('content')</div>
+
+	<div class="footer">@include('layouts.footer');</div>
+
+
+
+	<script src="/bower_components/jquery/dist/jquery.js"></script>
+	<script src="/bower_components/bootstrap/dist/js/bootstrap.js"></script>
+
+
+</body>
 </html>
